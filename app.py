4269f86befc3d935c746e73e28142c69a071cd44
Shashi Kumar <ayushmankasyap@gmail.com>
2024-06-30 21:20:50 +0530

    Updated App

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World! This is my Flask application running with Gunicorn on port 80."
+    return "Hello, World! This is Shashi."
 
 
 @app.route('/about')
 def about():
     return "This is the about page."
 
+# Run the Flask application if this file is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
--- startup.sh
+++ startup.sh
@@ -4,5 +4,6 @@ python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 cat requirements.txt
+echo "Shashi"
 # Start Gunicorn server with your Flask application
 gunicorn -b 0.0.0.0:80 -w 4 --timeout 600 app:app
