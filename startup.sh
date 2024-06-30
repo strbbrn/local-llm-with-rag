4e9b4f02a8f81e35e007f5ed3bcf159fb8ffb84b
Shashi Kumar <ayushmankasyap@gmail.com>
2024-06-30 21:47:43 +0530

    updated startup

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
+# python3 -m pip install --upgrade pip
+# pip install -r requirements.txt
 
-cat requirements.txt
-echo "Shashi"
-# Start Gunicorn server with your Flask application
-gunicorn -b 0.0.0.0:80 -w 4 --timeout 600 app:app
+# cat requirements.txt
+# echo "Shashi"
+# # Start Gunicorn server with your Flask application
+# gunicorn -b 0.0.0.0:80 -w 4 --timeout 600 app:app
