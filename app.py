b9d706fccdcc762b7fefbb033b53daad47ebf336
Shashi Kumar <ayushmankasyap@gmail.com>
2024-06-30 21:43:19 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World! This is Shashi."
+    return "Hello, Shashi."
 
 
 @app.route('/about')
 def about():
     return "Hello from about page."
 
+@app.route('/status')
+def status():
+    return jsonify({'status': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
