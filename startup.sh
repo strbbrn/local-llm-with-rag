388b10e2952f309ad3b8c0d5b3674ff62e5e9e72
Shashi Kumar <ayushmankasyap@gmail.com>
2025-01-09 16:18:35 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,9 +0,0 @@
-# #!/bin/bash
-
-# python3 -m pip install --upgrade pip
-# pip install -r requirements.txt
-
-# cat requirements.txt
-# echo "Shashi"
-# # Start Gunicorn server with your Flask application
-# gunicorn -b 0.0.0.0:80 -w 4 --timeout 600 app:app
